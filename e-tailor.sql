633bd07b8dcb210f93b26e47582b2085bde86689
ainunfauzi <44285058+ainunfauzi@users.noreply.github.com>
2020-05-18 09:21:33 +0700

    db change

--- e-tailor.sql
+++ e-tailor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2020 at 11:58 AM
+-- Generation Time: May 18, 2020 at 04:21 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -21,39 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `e-tailor`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jurusan`
---
-
-CREATE TABLE `jurusan` (
-  `id_jurusan` int(11) NOT NULL,
-  `nama_jurusan` varchar(1000) NOT NULL,
-  `keterangan` varchar(1000) NOT NULL,
-  `prospek_kerja` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `jurusan`
---
-
-INSERT INTO `jurusan` (`id_jurusan`, `nama_jurusan`, `keterangan`, `prospek_kerja`) VALUES
-(91911, 'Statistika', 'Mempelajari tentang matematika, ilmu ukur, aljabar, metode untuk membuat keputusan erdasarkan bukti-bukti yang berupa catatan bilangan.', 'Dapat Bekerja di lembaga penelutian, Biro pusat statistic, departemen telekomunikasi, dll.\r\nDapat bekerja di Industri Software dan Hardware seperti IBM, Microsoft, Lembaga Pemerintahan, Telkom, PLN, Biro Jasa Komsputer, dll.'),
-(91912, 'Teknik Informatika', 'Mempelajari bagaimana dasar-dasar coding, membuat aplikasi pemrograman software , jaringan, website, hingga programming.', 'Dapat bekerja di Industri Software dan Hardware seperti IBM, Microsoft, Lembaga Pemerintahan, Telkom, PLN, Biro Jasa Komputer, dll.\r\n'),
-(91913, 'Teknik Kimia', 'Mempelajari tentang kalkulus, termodinamika, kimia analitik, fluida, manajemen proyek, evaluasi ekonomi pabrik. Ilmu pengetahuan terapan perancangan, pembangunan dan pelaksanaan operasi dari pabrik-pabrik pengolah bahan mentah menjadi produk melalui kimiawi.', 'Dapat bekerja di Industri pengolahan minyak dan gas seperti Pertamina, Biro Konsultan dan Penelitian dll.'),
-(91914, 'Administrasi Bisnis', 'Mempelajari Semua hal yang berkaitan dengan operasional perusahaan bisnis. Selain bisnis, mahasiswa yang masuk dalam jurusan ini akan diajarkan berbisnis hingga menciptakan sebuah produk.', 'Bisa bekerja di perusahaan pemerintah (BUMN) atau swasta.'),
-(91915, 'Administrasi Keungan dann Perbankan', 'Mempelajari ilmu keuangan, manajemen surat, pengelolaan perusahaan hingga pembukuan transaksi perbankan dan lain-lain.', 'Dapat bekerjadi BUMN, instansi pemerintah, hingga perusahaan swasta.'),
-(91916, 'Agroteknologi', 'Mempelajari dasar-dasar ilmiah dalam menemukan, menganalisa hingga memecahkan masalah. Mahasiswa juga akan dibeklai dengan kemampuan akademik dalam menerapkan dan mengembangkan ilmu pengetahuan.', 'Dapat bekerja di Perkebunan, Instansi Pemerintah, Lembaga Swadaya Masyarakat dan Lain Sebagainya.'),
-(91917, 'Arkeologi', 'Mempelajari pendekatan sejarah. Mahasiswa akan mendapatkan pemahaman mengenai sejarah khususnya di masa kuno yang mana masyarakat belum mengenal angka dan huruf.', 'Dapat bekerja sebagai Arkeolog di Instansi Pemerintah, Sejarawan, Penulis Perjalanan, Teknisi Musum dan Konservator, Juru Arship hingga Antropologi.'),
-(91918, 'Arsitektur', 'Mempelajari beragam desain dan rancangankonstruksi bangunan. Mahasiswa cenderung menuangkan ide, konsep serta desain di atas kertas.', 'Dapat bekerja di ssemua perusahaan, Melihat Infrastruktur di Indonesia semakin hari semakin berkembang.'),
-(91919, 'Astronomi', 'Mempelajari tentang benda-benda langit. Menitikberatkan penerapan ilmu fisika.', 'Dapat menjadi Astronomer, Bekerja di Obsevarium atau Lembaga Antariksa.'),
-(919110, 'Biologi', 'Mempelajari hal tentang makhluk hidup mulai dari manusia, tumbuhan, hewan hingga mikroba. Mahasiswa akan diajarkan tentang seluk beluk makhluk hidup mulai dari unit terkecil hingga beragam spesies.', 'Dapat menjadi seorang tenaga pendidik, Peneliti Laboratorium, dll.'),
-(919111, 'Farmasi', 'Mempelajari dunia medis, khususnya obat-obatan. Struktur senyawa obat secara sintetisnya menjadikan sebuah obat yang bisa dikonsumsi.', 'Dapat bekerja menjadi seorang Apoteker, Laboratorium atau klinik.'),
-(919112, 'Hubungan Internasional', 'Mempelajari bagaimana berdiploma, memahami konflik sebuah Negara, politik internasional dan sebaagainya.', 'Dapat bekerja menjadi peneliti politik, penyiar berita, wartawan, konsultasi imigrasi, diplomat, dan konsultasi politik, dan berbagai instansi yang membutuhkan tenangaa ahli hubungan internasional.'),
-(919113, 'Ilmu Hukum', 'Mempelajari tentang semua hal mengenai hukum baik praktik ataupun teori.', 'Dapat menjadi seorang notaris, hakim, jaksa dan sebagainya.'),
-(919114, 'Teknik Elektro', 'Hubungan arus listrik', 'PLN, dll.');
 
 -- --------------------------------------------------------
 
@@ -80,119 +47,62 @@ INSERT INTO `tbl_admin` (`id_admin`, `nama_admin`, `username_admin`, `password_a
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_pengajar`
+-- Table structure for table `tbl_pesanan`
 --
 
-CREATE TABLE `tbl_pengajar` (
-  `id_pengajar` int(11) NOT NULL,
-  `nama_pengajar` varchar(50) NOT NULL,
-  `alamat` varchar(50) NOT NULL,
-  `username_pengajar` varchar(50) NOT NULL,
-  `password_pengajar` varchar(50) NOT NULL,
-  `id_grupbimbel` int(20) NOT NULL
+CREATE TABLE `tbl_pesanan` (
+  `id_pesanan` int(20) NOT NULL,
+  `jenis_pakaian` varchar(64) NOT NULL,
+  `ukuran` varchar(64) NOT NULL,
+  `warna` varchar(64) NOT NULL,
+  `jenis_kain` varchar(64) NOT NULL,
+  `jumlah` varchar(64) NOT NULL,
+  `tanggal_pemesanan` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_pengajar`
+-- Dumping data for table `tbl_pesanan`
 --
 
-INSERT INTO `tbl_pengajar` (`id_pengajar`, `nama_pengajar`, `alamat`, `username_pengajar`, `password_pengajar`, `id_grupbimbel`) VALUES
-(53551, 'Roni', 'Gresik', 'roni', 'roni', 1),
-(53552, 'Elma', 'Lamongan', 'elma', 'elma', 2),
-(53553, 'Yogi', 'Sukabumi', 'yogi', 'yogi', 3),
-(53554, 'Kevin', 'Lampung', 'kevin', 'kevin', 1),
-(53555, 'Sasa', 'Banyuwangi', 'sasa', 'sasa', 2),
-(53556, 'Sinta', 'Madiun', 'sinta', 'sinta', 3),
-(53557, 'Kartiko', 'Nganjuk', 'kartiko', 'kartiko', 1),
-(53558, 'Erna', 'Madiun', 'erna', 'erna', 2),
-(53559, 'Feni', 'Lampung', 'feni', 'feni', 3),
-(53560, 'Sultan', 'Surabaya', 'sultan', 'sultan', 1);
+INSERT INTO `tbl_pesanan` (`id_pesanan`, `jenis_pakaian`, `ukuran`, `warna`, `jenis_kain`, `jumlah`, `tanggal_pemesanan`) VALUES
+(1, 'hkh', 'jkh', 'kjhkjh', 'kh', 'kjh', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_siswa`
+-- Table structure for table `tbl_user`
 --
 
-CREATE TABLE `tbl_siswa` (
-  `id_siswa` int(20) NOT NULL,
-  `nama_siswa` varchar(64) NOT NULL,
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
   `tgl_lahir` date NOT NULL,
-  `asal_sekolah` varchar(64) NOT NULL,
-  `alamat` varchar(64) NOT NULL,
-  `email_siswa` varchar(64) NOT NULL,
-  `nohp_siswa` int(20) NOT NULL,
-  `id_grupbimbel` int(20) NOT NULL,
-  `username_siswa` varchar(64) NOT NULL,
-  `password_siswa` varchar(64) NOT NULL
+  `email_user` varchar(64) NOT NULL,
+  `nohp_user` int(20) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `username_user` varchar(50) NOT NULL,
+  `password_user` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_siswa`
+-- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_siswa` (`id_siswa`, `nama_siswa`, `tgl_lahir`, `asal_sekolah`, `alamat`, `email_siswa`, `nohp_siswa`, `id_grupbimbel`, `username_siswa`, `password_siswa`) VALUES
-(1101, 'Clarissa', '2002-09-01', 'SMAN 1 Gresik', 'Gresik', 'Clarissa11@gmail.com', 1119014, 1, 'clarissa', 'clarissa'),
-(1102, 'Rere', '2019-11-11', 'SMAN 1 Solo', 'Solo', 'Rere@gmail.com', 123677, 1, 'Rere', 'rere'),
-(1103, 'Farhan', '1999-08-17', 'SMAN 2 Semarang', 'Semarang', 'Farhan@gmail.com', 110328, 2, 'Farhan', 'Farhan'),
-(1104, 'Razka', '2000-06-17', 'SMAN 1 Bangkalan', 'Bangkalan', 'Razka@gmail.com', 1107928, 3, 'Razka', 'Razka'),
-(1105, 'Azzam', '1999-10-28', 'SMAN 1 Ponorogo', 'Ponorogo', 'Azzam@gmail.com', 117658, 2, 'Azzam', 'Azzam'),
-(1106, 'Riki', '2000-04-07', 'SMAN 2 Tanggerang', 'Tanggerang', 'Riki@gmail.com', 116786, 3, 'Riki', 'Riki'),
-(1107, 'Friska', '1999-12-11', 'SMAN 3 Surabaya', 'Surabaya', 'Frizka@gmail.com', 118765, 3, 'Frizka', 'Frizka'),
-(1108, 'Yolanda', '1999-09-20', 'SMAN 3 MAlang', 'Malang', 'Yolanda@gmail.com', 114567, 2, 'Yolanda', 'Yolanda'),
-(1109, 'Citra', '1999-08-30', 'SMAN 4 Batu', 'Batu', 'Citra@gmail.com', 110900, 3, 'Citra', 'Citra'),
-(1110, 'Shofia', '1999-01-14', 'SMAN 4 Jakarta', 'Jakarta', 'Shofia@gmail.com', 113782, 3, 'Shofia', 'Shofia'),
-(1111, 'aadadw', '2020-04-15', 'dwdwqd', 'vgsb', 'wefwef', 80799, 2, 'dawdq', '423rqdx');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `universitas`
---
-
-CREATE TABLE `universitas` (
-  `id_universitas` int(11) NOT NULL,
-  `nama_universitas` varchar(60) NOT NULL,
-  `total_prodi` int(11) NOT NULL,
-  `alamat` varchar(60) NOT NULL,
-  `deskripsi` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `universitas`
---
-
-INSERT INTO `universitas` (`id_universitas`, `nama_universitas`, `total_prodi`, `alamat`, `deskripsi`) VALUES
-(1, 'Universitas Trunojoyo Madura', 24, 'Madura', 'Terakreditasi Baik  Sekali'),
-(2, '', 25, 'Banda Aceh', 'Terakreditasi Baik Sekali'),
-(3, 'Universitas Negeri Medan', 25, 'Meulaboh', 'Terakreditasi Baik Sekali'),
-(4, 'Universitas Airlangga', 28, 'Surabaya', 'Terakreditasi Baik Sekali'),
-(5, 'Universitas Sriwijaya', 26, 'Palembang', 'Terakreditasi Baik Sekali'),
-(6, 'Universitas Lampung', 27, 'Bandar Lampung', 'Terakreditasi Baik Sekali'),
-(7, 'Universitas Sultan Agung Tirtayasa', 26, 'Serang Banten', 'Terakreditasi Baik Sekali'),
-(8, 'Universitas Indonesia', 30, 'Jakarta', 'Terakreditasi Baik Sekali'),
-(9, 'Universitas Pembangunan Nasional Veteran Jakarta', 27, 'Pondok Labu', 'Terakreditasi Baik Sekali'),
-(10, 'Institut Pertanian Bogor', 29, 'Bogor', 'Terakreditasi Baik Sekali'),
-(11, 'Universitas Negeri Semarang', 26, 'Semarang', 'Terakreditasi Baik Sekali'),
-(12, 'Universitas Sebelas Maret', 28, 'Surakarta', 'Terakreditasi Baik Sekali'),
-(13, 'Universitas Gadjah Mada', 28, 'Yogyakarta', 'Terakreditasi Baik Sekali'),
-(14, 'Universitas  Negeri Jember', 29, 'Jember', 'Terakreditasi Baik Sekali'),
-(15, 'Universitas Brawijaya', 30, 'Malang', 'Terakreditasi Baik Sekali'),
-(16, 'Universitas Negeri Surabaya', 29, 'Surabaya', 'Terakreditasi Baik Sekali'),
-(17, 'Universitas Padjajaran', 24, 'Bandung', 'Terakreditasi Baik Sekali'),
-(18, 'Universitas Siliwangi', 26, 'Tasikmalaya', 'Terakreditasi Baik Sekali'),
-(19, 'Universitas Diponegoro', 27, 'Semarang', 'Terakreditasi Baik Sekali'),
-(20, 'Universitas Jendral Sudirman', 28, 'Purwokerto', 'Terakreditasi Baik Sekali');
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `tgl_lahir`, `email_user`, `nohp_user`, `alamat`, `username_user`, `password_user`) VALUES
+(53551, 'Roni', '0000-00-00', '', 0, 'Gresik', 'roni', 'roni'),
+(53552, 'Elma', '0000-00-00', '', 0, 'Lamongan', 'elma', 'elma'),
+(53553, 'Yogi', '0000-00-00', '', 0, 'Sukabumi', 'yogi', 'yogi'),
+(53554, 'Kevin', '0000-00-00', '', 0, 'Lampung', 'kevin', 'kevin'),
+(53555, 'Sasa', '0000-00-00', '', 0, 'Banyuwangi', 'sasa', 'sasa'),
+(53556, 'Sinta', '0000-00-00', '', 0, 'Madiun', 'sinta', 'sinta'),
+(53557, 'Kartiko', '0000-00-00', '', 0, 'Nganjuk', 'kartiko', 'kartiko'),
+(53558, 'Erna', '0000-00-00', '', 0, 'Madiun', 'erna', 'erna'),
+(53559, 'Feni', '0000-00-00', '', 0, 'Lampung', 'feni', 'feni'),
+(53560, 'Sultan', '0000-00-00', '', 0, 'Surabaya', 'sultan', 'sultan');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `jurusan`
---
-ALTER TABLE `jurusan`
-  ADD PRIMARY KEY (`id_jurusan`);
 
 --
 -- Indexes for table `tbl_admin`
@@ -201,32 +111,20 @@ ALTER TABLE `tbl_admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indexes for table `tbl_pengajar`
+-- Indexes for table `tbl_pesanan`
 --
-ALTER TABLE `tbl_pengajar`
-  ADD PRIMARY KEY (`id_pengajar`);
+ALTER TABLE `tbl_pesanan`
+  ADD PRIMARY KEY (`id_pesanan`);
 
 --
--- Indexes for table `tbl_siswa`
+-- Indexes for table `tbl_user`
 --
-ALTER TABLE `tbl_siswa`
-  ADD PRIMARY KEY (`id_siswa`);
-
---
--- Indexes for table `universitas`
---
-ALTER TABLE `universitas`
-  ADD PRIMARY KEY (`id_universitas`);
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `jurusan`
---
-ALTER TABLE `jurusan`
-  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=919115;
 
 --
 -- AUTO_INCREMENT for table `tbl_admin`
@@ -235,22 +133,16 @@ ALTER TABLE `tbl_admin`
   MODIFY `id_admin` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `tbl_pengajar`
+-- AUTO_INCREMENT for table `tbl_pesanan`
 --
-ALTER TABLE `tbl_pengajar`
-  MODIFY `id_pengajar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53561;
+ALTER TABLE `tbl_pesanan`
+  MODIFY `id_pesanan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `tbl_siswa`
+-- AUTO_INCREMENT for table `tbl_user`
 --
-ALTER TABLE `tbl_siswa`
-  MODIFY `id_siswa` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1112;
-
---
--- AUTO_INCREMENT for table `universitas`
---
-ALTER TABLE `universitas`
-  MODIFY `id_universitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53562;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
